57b33f495c19e67c25dec48207d0295b4c14ae22
quro4ka <quro4ka@gmail.com>
2023-07-05 21:44:08 +0300

    replace to webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './fonts.css'
+import './reset.css'
+import './styles.css'
+
+const root = ReactDOM.createRoot(document.getElementById('app'))
+root.render(
+  <>
+    <App />
+  </>,
+)
